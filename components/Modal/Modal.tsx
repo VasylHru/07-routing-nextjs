14c39eeb3ea4690a1713ec8ea8d09a563b257fd4
Vasyl Hrubyi <120045095+VasylHru@users.noreply.github.com>
2025-12-06 12:55:18 +0200

    Initial commit

---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
+import css from "./Modal.module.css";
+
+interface ModalProps {
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+ const Modal = ({ onClose, children }: ModalProps) => {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        onClose();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [onClose]);
+
+  return createPortal(
+    <div className={css.backdrop} onClick={onClose}>
+      <div className={css.modal} onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>,
+    document.body
+  );
+};
+
+
+export default Modal
