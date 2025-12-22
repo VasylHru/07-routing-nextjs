bf7188acb333fefa597c31d0c7252b51d2af47e4
Vasyl Hrubyi <ronaldomen7@gmail.com>
2025-12-22 14:17:16 +0200

    delit-filter-remove-notelist

--- app/notes/filter/[...slug]/Notes.client.tsx
+++ app/notes/filter/[...slug]/Notes.client.tsx
@@ -5,28 +5,30 @@ import { useQuery } from "@tanstack/react-query";
 import { useDebounce } from "use-debounce";
 
 import { fetchNotes } from "@/lib/api";
-
+import Link from "next/link";
 import NoteList from "@/components/NoteList/NoteList";
 import SearchBox from "@/components/SearchBox/SearchBox";
 import Pagination from "@/components/Pagination/Pagination";
-import Modal from "@/components/Modal/Modal";
-import NoteForm from "@/components/NoteForm/NoteForm";
 import Loader from "@/components/Loader/Loader";
 import ErrorMessage from "@/components/ErrorMessage/ErrorMessage";
 import css from "@/components/NotesPage/NotesPage.module.css";
 
-export default function NotesClient() {
+
+type NotesClientProps = {
+   tag?: string;
+};
+
+export default function NotesClient({ tag }: NotesClientProps) {
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState("");
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const [debouncedSearch] = useDebounce(search, 500);
+  const [debouncedSearch] = useDebounce(search,500);
 
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["notes", page, debouncedSearch],
-    queryFn: () => fetchNotes(page, debouncedSearch),
+ const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["notes",tag, page, debouncedSearch],
+    queryFn: () => fetchNotes(page,tag,debouncedSearch),
     placeholderData: (prev) => prev,
-  });
+  }); 
 
   const handleSearchChange = (value: string) => {
     setSearch(value);
@@ -46,20 +48,14 @@ export default function NotesClient() {
           />
         )}
         <div className={css.sidebar}></div>
-        <button className={css.button} onClick={() => setIsModalOpen(true)}>
-          Create note +
-        </button>
+        <Link href="/notes/action/create">
+          <button className={css.button}>Create note +</button>
+        </Link>
       </header>
       {isLoading && <Loader />}
       {isError && <ErrorMessage message={(error as Error).message} />}
 
       {data && data.notes.length > 0 && <NoteList notes={data.notes} />}
-
-      {isModalOpen && (
-        <Modal onClose={() => setIsModalOpen(false)}>
-          <NoteForm onCancel={() => setIsModalOpen(false)} />
-        </Modal>
-      )}
     </div>
   );
 }
--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -3,29 +3,66 @@ import {
   QueryClient,
   dehydrate,
 } from "@tanstack/react-query";
+import type { Metadata } from "next";
 import { fetchNotes } from "@/lib/api";
-import NotesClient from "@/app/notes/Notes.client";
+import NotesClient from "./Notes.client";
+import { notFound } from "next/navigation";
+
+type PageProp = {
+  params: Promise<{ slug: string[] }>;
+};
+
+export const generateMetadata = async ({
+  params,
+}: PageProp): Promise<Metadata> => {
+  const { slug } = await params;
+  const tagParam = slug?.[0];
+  const normalizedTag = tagParam === "all" ? `All notes` : tagParam;
+
+  return {
+    title: `${normalizedTag}`,
+    description: `Notes page filtered by tag ${normalizedTag}`,
+    openGraph: {
+      title: `${normalizedTag}`,
+      description: `Notes page filtered by tag ${normalizedTag}`,
+      url: `https://08-zustand-blush-seven.vercel.app/notes/filter/${tagParam}`,
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: `NoteHub Page`,
+        },
+      ],
+    },
+  };
+};
 
 export default async function NotesFilterPage({
   params,
 }: {
-  params: Promise<{ tag: string[] }>;
+  params: Promise<{ slug: string[] }>;
 }) {
   const queryClient = new QueryClient();
 
-  const { tag } = await params;
+  const { slug } = await params;
 
-  const tagParam = tag?.[0];
+  const VALID_TAGS = ["all", "Todo", "Work", "Personal", "Meeting", "Shopping"];
+  const tagParam = slug?.[0];
   const normalizedTag = tagParam === "all" ? undefined : tagParam;
 
+  if (!VALID_TAGS.includes(tagParam)) {
+    notFound();
+  }
+
   await queryClient.prefetchQuery({
-   queryKey: ["notes", "filter", tag ?? "all"],
-    queryFn: () => fetchNotes(1, undefined, normalizedTag),
+    queryKey: ["notes", normalizedTag, 1, ""],
+    queryFn: () => fetchNotes(1, normalizedTag, ""),
   });
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <NotesClient />
+      <NotesClient tag={normalizedTag} />
     </HydrationBoundary>
   );
 }
